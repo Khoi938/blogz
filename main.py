b3ad16a15cba50ea22bbfa4d243752a4c1f61a46
Khoi Le <eclipse938@gmail.com>
2018-01-24 12:43:47 -0500

    saving

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:123456@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:123456@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 
 database = SQLAlchemy(app)
@@ -15,21 +15,26 @@ class Blog(database.Model):
     id = database.Column(database.Integer, primary_key = True)
     title = database.Column(database.String(100))
     body = database.Column(database.Text)
+    owner_id = database.Column(database.Integer, forg)
 
     def __init__(self,title,body):
         self.title = title
         self.body = body
 
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key = True
+    username = database.Column(database.String(15))
+    password =database.Column(database.String(10))
+    blogs = database.Column(database.refernce)
+
 
 
 
 @app.route('/blog', methods=['POST', 'GET'])
-def main():
-    
+def main():  
     all_posts = Blog.query.all()
     all_posts.reverse()
     return render_template('blog_post.html',top = 'My Blog.', posts = all_posts)
-
 
 @app.route('/new_post', methods = ['POST','GET'])
 def newpost():
@@ -49,8 +54,7 @@ def single():
         htmlSTR = str(new_post.id)
         
         return redirect('/single_post?id='+htmlSTR)
-        #return render_template('single_post.html', title = new_post.title, body = new_post.id)
-
+        
 
     post_id = int(request.args.get('id'))
     stuff = Blog.query.get(post_id)
